03a601a77d77be88146a42e5768341dab958bc2f
rahulray281528-star <rahulray281528@gmail.com>
2026-09-03 11:47:01 +0530

    Add: User Response DTO

---
+++ auth-service/src/main/java/com/enterprise/auth/dto/UserResponse.java
@@ -0,0 +1,20 @@
+package com.enterprise.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String id;
+    private String username;
+    private String email;
+    private String role;
+    private LocalDateTime createdAt;
+}
